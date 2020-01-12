d715012f7081a55c5738b860be515d91f2df1676
Stanley Ndunwere <StanleyNdunwere@gmail.com>
2020-01-12 13:37:30 +0100

    Empty SpringBoot project

---
+++ src/main/java/com/centdom/hibernate_relational_mapping/HibernateRelationalMappingApplication.java
@@ -0,0 +1,13 @@
+package com.centdom.hibernate_relational_mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateRelationalMappingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HibernateRelationalMappingApplication.class, args);
+    }
+
+}
